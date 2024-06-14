14e58ab5761df0248f447c14fd7a5086b8f403bd
Nandana Nagabhushana <agasthya.a13@gmail.com>
2024-06-14 05:12:32 +0000

    added tests, macros, packages

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -7,7 +7,9 @@
 WITH src_reviews AS (
 SELECT * FROM {{ ref('src_reviews') }}
 )
-SELECT * FROM src_reviews
+SELECT 
+{{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name']) }} as review_id,
+* FROM src_reviews
 WHERE review_text is not null
 {% if is_incremental() %}
     -- this filter will only be applied on an incremental run
--- tests/consistent_created_at.sql
+++ tests/consistent_created_at.sql
@@ -5,5 +5,5 @@ d_l as (
     select * from {{ ref('dim_listings_cleansed') }}
 )
 select f_r.* 
-from f_r join d_l
-where f_r.review_date >= d_l.created_at
+from f_r inner join d_l using (listing_id)
+where f_r.review_date < d_l.created_at
